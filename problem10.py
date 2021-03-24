72b57f45af67957a8e26705f1572955227abc527
thepatrat <71664904+thepatrat@users.noreply.github.com>
2021-03-24 08:24:53 +0100

    testing branch

--- problem10.py
+++ problem10.py
@@ -7,40 +7,55 @@ startTime = datetime.now()
 
 def _generate_list(max_number):
     """ Creates a list of integers from 1 to the given number """
-    integers_to_n = [2]
+    integers_to_n = []
     for i in range(3, max_number + 1, 2):
         integers_to_n.append(i)
     return integers_to_n
 
-def sieving(integer_list):
-    """ Uses the eukledian prime sieving method to delete non primes from 
-    a list of integers """
-
-    # find the end number
-    end_number = int(math.sqrt(integer_list[-1]))
-    # loop that provides the numbers, of which we are deleting the 
-    # multiples.
-        # tqdm shows us a progress bar of the loop
-    for number in tqdm(range(3, end_number)):
-        # only runs it if the number has not yet been deleted from our integer list
-        # therefore only checks for the remaining numbers ( = primes)
-        if number in integer_list:
-            # checks if the elements in the integer list can be divided by the 
-            # integer and if they can it deletes them
-            for n in integer_list:
-                if n % number == 0 and n != number: 
-                    integer_list.remove(n)
-        else:
-            # if the number was already deleted it passes and goes to the next
+def _remove_multiples(integer_list, number):
+    """ Removes all the multiples of a number from a list 
+    of integers """
+    # checks what the maximum number in the list is
+    highest_number = max(integer_list)
+    # we start with a multiple of 2 for each number
+    n = 2
+    # the multiple of the number cant exceede the maximum number
+    while n * number <= highest_number:
+        print(n*number)
+        # we expect an error, as e.g. 6 is already deleted 
+        # because it is a multiple of 2
+        try:
+            integer_list.remove(number * n)
+        except:
             pass
-        # now it runs again over the shortened list
+        # we go to the next multiple of the number
+        n += 1
     return integer_list
 
-# creates our prime list until the max number
+def _sieving(integer_list):
+    # create a list to store our primes in
+    primes = []
+    # repeat our multiple removing until our list is empty
+    while integer_list:        
+        # check lowest number in the list
+        lowest_number = integer_list[0]
+        # lowest number must be a prime so safe it in the primes list
+        primes.append(lowest_number)
+        # now remove all the multiples of the lowest number from our integers
+        integer_list = _remove_multiples(integer_list, lowest_number)
+        # to get a new lowest number, we delete the old one
+        # could be put in remove multiples function, but I want to 
+        # keep that one only for multiples
+        integer_list.remove(lowest_number)
+    return primes
+
+
+
+# creates our prime list until the max number and use the sieving method on
 max_number = 2_000_000
-prime_list = sieving(_generate_list(max_number))
+prime_list = _sieving(_generate_list(max_number))
 
 # gives the results
 print(f"\n\nWe found {len(prime_list)} primes. The sum over these primes "
     f"is {sum(prime_list)}.")
-print(f"The caluclation took {datetime.now() - startTime} minutes.")
+print(f"The caluclation took {datetime.now() - startTime}.")
